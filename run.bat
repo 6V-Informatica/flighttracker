60e398c3a72248028a53f34e545f66f5604212be
Brummelhuis <brummelhuis135857@hetassink.nl>
2022-09-21 14:12:26 +0200

    Made some changes

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-cd D:\projects\voorbeeld-myfirstapp
-"D:\git\bin\git.exe" add --all
-"D:\git\bin\git.exe" commit -m "Made some changes"
-"D:\git\bin\git.exe" push origin main
+cd E:\projects\flighttracker
+"E:\git\bin\git.exe" add --all
+"E:\git\bin\git.exe" commit -m "Made some changes"
+"E:\git\bin\git.exe" push origin main
 pause
